4e335da133da2827a6a2ab490124a3f09e16981f
danielmbutler <danielmbutler@hotmail.co.uk>
2020-10-13 21:14:51 +0100

    Added Average Score function

--- App.py
+++ App.py
@@ -11,10 +11,19 @@ def Home_or_Away(Home,Away):
     #print(query)
     cursor.execute(query)
     for row in cursor:
-        print(row)
         if 'A' in (row):
             print(Away + ' Win')
         if 'H' in (row):
             print(Home + ' Win')
 
+def Average_Score(Home,Away):
+        query = 'SELECT TOP 1 [EFL-1888-2017].[dbo].[england].FT FROM [EFL-1888-2017].[dbo].[england] WHERE home=\'' + Home + '\' AND visitor=\'' + Away + '\' GROUP BY [EFL-1888-2017].[dbo].[england].FT ORDER  BY COUNT(*) DESC'
+        cursor = conn.cursor()
+        #print(query)
+        cursor.execute(query)
+        for row in cursor:
+            print("average result", row)
+
+
 Home_or_Away('Arsenal','Manchester United')
+Average_Score('Arsenal','Manchester United')
